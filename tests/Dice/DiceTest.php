b1805bde7665204ae5061475fd8b5bd3f71d608b
Dustin Moorman <dmoorman@softlayer.com>
2016-04-04 23:04:12 -0500

    DiceTest improvements

--- tests/Dice/DiceTest.php
+++ tests/Dice/DiceTest.php
@@ -7,14 +7,30 @@ class DiceTest extends \PHPUnit_Framework_TestCase
     public function testRoll()
     {
         $dice = new \Dustinmoorman\Dice\Dice(5);
-        $result = $dice->roll();
 
-        $this->assertInternalType('int', $result);
-
-        //50 chances to go out of range
         for ($rolls = 0; $rolls <= 50; $rolls++) {
+            $result = $dice->roll();
+
+            $this->assertInternalType('int', $result);
+            $this->assertTrue($result <= 5);
+            $this->assertTrue($result >= 1);
             $this->assertEquals(1, preg_match('#^[1-5]$#', $result));
         }
+    }
+
+    public function testSetSides()
+    {
+        $dice = new \Dustinmoorman\Dice\Dice();
+        $dice->setSides(20);
+
+        for ($rolls = 0; $rolls <= 50; $rolls++) {
+            $result = $dice->roll();
+
+            $this->assertInternalType('int', $result);
+            $this->assertTrue($result <= 20);
+            $this->assertTrue($result >= 1);
+        }
+
     }
 
     /**
